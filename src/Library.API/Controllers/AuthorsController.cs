490a699fb529b17d4fe437ec18987da861570d5b
DoCaoTri <docaotri211997@gmail.com>
2019-01-24 15:41:26 +0700

    init

---
+++ src/Library.API/Controllers/AuthorsController.cs
@@ -0,0 +1,42 @@
+﻿using Library.API.Services;
+using Library.API.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using Library.API.Helpers;
+using AutoMapper;
+
+namespace Library.API.Controllers
+{
+    [Route("api/authors")]
+    public class AuthorsController: Controller
+    {
+        private ILibraryRepository _libraryRepository;
+
+        public AuthorsController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        [HttpGet()]
+        public IActionResult GetAuthors(bool includeBooks = false)
+        {
+            var authorsFromRepo = _libraryRepository.GetAuthors(includeBooks);
+
+            var authors = Mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo);
+
+            return Ok(authors);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetAuthor(Guid id)
+        {
+            var authorFromRepo = _libraryRepository.GetAuthor(id);
+            if(authorFromRepo == null)
+            {
+                return NotFound();
+            }
+            return Ok(Mapper.Map<AuthorDto>(authorFromRepo));
+        }
+    }
+}
---
+++ src/Library.API/Models/AuthorDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.API.ViewModels
+{
+    public class AuthorDto
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Genre { get; set; }
+
+        public ICollection<BookDto> Books { get; set; }
+           = new List<BookDto>();
+
+    }
+}
